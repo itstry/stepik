07507ff6ebd9fd78cbf58fc50cd69c232ad5522c
itstry <98533960+itstry@users.noreply.github.com>
2022-06-24 00:56:24 +0300

    Пересечение отрезков

--- temp/for_examples.py
+++ temp/for_examples.py
@@ -1,9 +1,20 @@
-def main():
-    return {letter: index for index, letter in enumerate('qwertyuiop', 1)}
-
-
-if __name__ == '__main__':
-    x = main()
-    z = {b: a for a, b in x.items()}
-    print(x)
-    print(z)
+a1, b1, a2, b2, space = int(input()), int(input()), int(input()), int(input()), ' '
+if (b1 < a2 and a1 < b2) or (b2 < a1 and a2 < b1):
+    print('пустое множество')
+elif b1 == a2 or a1 == b2:
+    print(a1 if (a1 == b2) else a2)
+elif a1 < a2:
+    if b1 > b2 or b1 == b2:
+        print(a2, b2)
+    if b1 < b2:
+        print(a2, b1)
+elif a1 > a2:
+    if b1 > b2:
+        print(a1, b2)
+    if b1 < b2 or b1 == b2:
+        print(a1, b1)
+elif a1 == a2:
+    if b1 == b2 or b1 < b2:
+        print(a1, b1)
+    if b1 > b2:
+        print(a2, b2)
