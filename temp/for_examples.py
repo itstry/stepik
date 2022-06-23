c293135a15eaf3ca5aec661152b49746b7343290
itstry <98533960+itstry@users.noreply.github.com>
2022-06-24 01:00:55 +0300

    Пересечение отрезков v2

--- temp/for_examples.py
+++ temp/for_examples.py
@@ -3,18 +3,13 @@ if (b1 < a2 and a1 < b2) or (b2 < a1 and a2 < b1):
     print('пустое множество')
 elif b1 == a2 or a1 == b2:
     print(a1 if (a1 == b2) else a2)
-elif a1 < a2:
+elif a1 < a2 or a1 == a2:
     if b1 > b2 or b1 == b2:
         print(a2, b2)
     if b1 < b2:
         print(a2, b1)
-elif a1 > a2:
-    if b1 > b2:
-        print(a1, b2)
+elif a1 > a2 or a1 == a2:
     if b1 < b2 or b1 == b2:
         print(a1, b1)
-elif a1 == a2:
-    if b1 == b2 or b1 < b2:
-        print(a1, b1)
     if b1 > b2:
-        print(a2, b2)
+        print(a1, b2)
