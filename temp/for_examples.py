e7fe1464916a0ba787fee7f5434dff6017ca18d7
itstry <98533960+itstry@users.noreply.github.com>
2022-06-24 01:17:07 +0300

    Пересечение отрезков v3

--- temp/for_examples.py
+++ temp/for_examples.py
@@ -1,15 +1,9 @@
 a1, b1, a2, b2, space = int(input()), int(input()), int(input()), int(input()), ' '
-if (b1 < a2 and a1 < b2) or (b2 < a1 and a2 < b1):
+if (a2 > b1 and a1 < b2) or (a1 > b2 and a2 < b1):
     print('пустое множество')
 elif b1 == a2 or a1 == b2:
     print(a1 if (a1 == b2) else a2)
-elif a1 < a2 or a1 == a2:
-    if b1 > b2 or b1 == b2:
-        print(a2, b2)
-    if b1 < b2:
-        print(a2, b1)
-elif a1 > a2 or a1 == a2:
-    if b1 < b2 or b1 == b2:
-        print(a1, b1)
-    if b1 > b2:
-        print(a1, b2)
+elif a1 <= a2:
+    print(str(a2) + space + str(b2) if (b1 >= b2) else str(a2) + space + str(b1))
+elif a1 >= a2:
+    print(str(a1) + space + str(b1) if (b1 <= b2) else str(a1) + space + str(b2))
