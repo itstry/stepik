29916ec95120c8ce4518f0cbedbc284d04dbf99a
itstry <98533960+itstry@users.noreply.github.com>
2022-06-25 20:27:59 +0300

    Геометрическая прогрессия 2.5 Целочисленная арифметика. Часть 2 https://stepik.org/lesson/284816/step/6?unit=266160

--- temp/for_examples.py
+++ temp/for_examples.py
@@ -1,5 +1,3 @@
-# 1 день; +20 задача; всего: 20
-x = int(input())
-for z in range(1, 5):
-    print(x * z, end='---')
-print(x * 5)
+# 1 день; +21 задача; всего: 21
+z, x, c = int(input()), int(input()), int(input())
+print(z * x ** (c - 1))
