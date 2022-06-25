6984a4d31ad3e3e4925eb30ac1aa04dbb2721c3c
itstry <98533960+itstry@users.noreply.github.com>
2022-06-25 22:28:20 +0300

    Наибольшие числа 🌶️🌶️ 7.3 Частые сценарии https://stepik.org/lesson/294243/step/1?unit=275922

--- pokolenye_python/for_examples.py
+++ pokolenye_python/for_examples.py
@@ -1,10 +1,11 @@
-# 5 day; 18 задача; total: 115
+# 5 day; 19 задача; total: 116
 # ^-^ в профиле ссылка гитхаб
-j = int(input())
-result_1 = 0
-result_2 = 0
-for i in range(1, j + 1, 2):
-    result_1 += i
-for i in range(2, j + 1, 2):
-    result_2 += i
-print(result_1-result_2)
+
+numbers_count = int(input())
+numbers_list = []
+
+for i in range(1, numbers_count+1):
+    numbers_list.append(int(input()))
+k = sorted(numbers_list)
+print(k[numbers_count-1])
+print(k[numbers_count-2])
