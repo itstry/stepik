4230e755d22915f8e92ba218dd8f260fad413872
itstry <98533960+itstry@users.noreply.github.com>
2022-06-25 21:08:11 +0300

    Квадратное уравнение 🌶️🌶️ 6.3 Модуль math https://stepik.org/lesson/265110/step/2?unit=246058

--- temp/for_examples.py
+++ temp/for_examples.py
@@ -1,6 +1,16 @@
-# 4 день; +3 задача; всего: 91
-# 4 день; +38 задача; всего: 142
-from math import floor, ceil
+# 4 день; +4 задача; всего: 92
 
-i = float(input())
-print(ceil(i) + floor(i))
+
+from math import sqrt
+
+a, b, c = float(input()), float(input()), float(input())
+discriminant = b * b - 4 * a * c
+if discriminant < 0:
+    print('Нет корней')
+elif discriminant == 0:
+    print(-b / (2 * a))
+elif discriminant > -0:
+    x1 = (-b + sqrt(discriminant)) / (2 * a)
+    x2 = (-b - sqrt(discriminant)) / (2 * a)
+    quar_list = [x1, x2]
+    print(f'{min(quar_list)}\n{max(quar_list)}')
