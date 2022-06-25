16d53457c2d8ccb4b82a820ac5fa82d2e5fab67f
itstry <98533960+itstry@users.noreply.github.com>
2022-06-25 21:03:00 +0300

    What's Your Name?
    6.2 Строковый тип данных
    https://stepik.org/lesson/265115/step/1?unit=246063

--- temp/for_examples.py
+++ temp/for_examples.py
@@ -1,6 +1,3 @@
 # хз какой день; +80 задача; всего: 80
 
-number_list = []
-for i in range(int(input())):
-    number_list.append(int(input()))
-print(sum(number_list))
+print(f'Hello {input()} {input()}! You just delved into Python')
