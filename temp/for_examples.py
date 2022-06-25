e80bad93974be22c95ecd2ad89930a1c2f6aa951
itstry <98533960+itstry@users.noreply.github.com>
2022-06-25 20:54:33 +0300

    Ход ферзя экзамен 5.1 Задачи на программирование https://stepik.org/lesson/292172/step/1?unit=273659

--- temp/for_examples.py
+++ temp/for_examples.py
@@ -1,8 +1,9 @@
-# 2 день; +65 задача; всего: 65
+# 2 день; +66 задача; всего: 66
 
-x1, y1, x2, y2 = int(input()), int(input()), int(input()), int(input())
+x1, y1 = int(input()), int(input())
+x2, y2 = int(input()), int(input())
 
-if (x1 + x2 + y1 + y2) % 2 != 0 and x1 != x2 and y1 != y2:
+if (x1 == x2 and y1 != y2) or (x1 != x2 and y1 == y2) or ((x1 - x2) ** 2 == (y1 - y2) ** 2):
     print('YES')
 else:
     print('NO')
