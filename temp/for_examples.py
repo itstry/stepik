88c39860af1df8e645d1d939b2a0d172a929ea7b
itstry <98533960+itstry@users.noreply.github.com>
2022-06-25 20:50:23 +0300

    Самописный калькулятор 🌶️ 4.3 Вложенные и каскадные условия https://stepik.org/lesson/265082/step/1?unit=246030

--- temp/for_examples.py
+++ temp/for_examples.py
@@ -1,12 +1,21 @@
-# 2 день; +55 задача; всего: 55
-def func(x):
-    if (i := x) < 60:
-        print('Легкий вес')
-    elif i < 64:
-        print('Первый полусредний вес')
-    elif i < 69:
-        print('Полусредний вес')
+# 2 день; +56 задача; всего: 56
+operator_list = ['-', '+', '*', '/']
+
+
+def calc(x: int, y: int, operator: str) -> any:
+    if y == 0 and operator == '/':
+        return 'На ноль делить нельзя!'
+    elif operator == operator_list[0]:
+        return x - y
+    elif operator == operator_list[1]:
+        return x + y
+    elif operator == operator_list[2]:
+        return x * y
+    elif operator == operator_list[3]:
+        return x / y
+    elif operator != any(operator_list):
+        return 'Неверная операция'
 
 
 if __name__ == '__main__':
-    func(int(input()))
+    print(calc(int(input()), int(input()), str(input())))
