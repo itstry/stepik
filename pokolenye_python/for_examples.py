75f4787e0dbb57481a583d01ae908c6a83c60b9b
itstry <98533960+itstry@users.noreply.github.com>
2022-06-25 22:13:17 +0300

    Сумма делителей 7.3 Частые сценарии https://stepik.org/lesson/294243/step/1?unit=275922

--- pokolenye_python/for_examples.py
+++ pokolenye_python/for_examples.py
@@ -1,9 +1,8 @@
-# 5 day; 16 задача; total: 113
+# 5 day; 17 задача; total: 114
 # ^-^ в профиле ссылка гитхаб
-
-result = 1
-for i in range(10):
-    j = int(input())
-    if j != 0:
-        result *= j
+j = int(input())
+result = 0
+for i in range(1, j + 1):
+    if j % i == 0:
+        result += i
 print(result)
