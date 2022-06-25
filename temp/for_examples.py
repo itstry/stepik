c611c2fcb02081893a62ed2e03b5959ce2b5fe25
itstry <98533960+itstry@users.noreply.github.com>
2022-06-25 20:47:21 +0300

    Гонка спидстеров 4.3 Вложенные и каскадные условия https://stepik.org/lesson/265082/step/1?unit=246030

--- temp/for_examples.py
+++ temp/for_examples.py
@@ -1,9 +1,9 @@
-# 1 день; +50 задача; всего: 50
-x1, y1 = int(input()), int(input())
-x2, y2 = int(input()), int(input())
+# 2 день; +51 задача; всего: 51
+zoom, flash = int(input()), int(input())
 
-if ((y1 + 1 == y2 or y1 - 1 == y2) and (x1 - 1 == x2 or x1 == x2 or x1 + 1 == x2)) or (
-        y2 == y1 and (x1 - 1 == x2 or x1 + 1 == x2)):
+if zoom > flash:
+    print('NO')
+elif flash > zoom:
     print('YES')
 else:
-    print('NO')
+    print("Don't know")
