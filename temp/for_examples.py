bd57fc1027f88f00fef33179aad1fd64058b6289
itstry <98533960+itstry@users.noreply.github.com>
2022-06-25 20:53:00 +0300

    Girls only экзамен 5.1 Задачи на программирование https://stepik.org/lesson/292172/step/1?unit=273659

--- temp/for_examples.py
+++ temp/for_examples.py
@@ -1,18 +1,12 @@
-# 2 день; +60 задача; всего: 60
+# 2 день; +61 задача; всего: 61
 
-x1, y1, x2, y2 = int(input()), int(input()), int(input()), int(input())
+year = int(input())
+gender = str(input())
 
-if (x1 + y1) % 2 == 0:
-    z1 = 1
-else:
-    z1 = 2
-
-if (x2 + y2) % 2 == 0:
-    z2 = 1
-else:
-    z2 = 2
-
-if z1 == z2:
-    print('YES')
+if 10 <= year <= 15:
+    if gender == 'f':
+        print('YES')
+    else:
+        print('NO')
 else:
     print('NO')
