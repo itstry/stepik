df09d7db559cd1090ac5d955023a5eda5dabcf91
itstry <98533960+itstry@users.noreply.github.com>
2022-06-25 20:39:51 +0300

    Наименьшее из двух чисел 4.1 Выбор из двух https://stepik.org/lesson/265081/step/1?unit=246029

--- temp/for_examples.py
+++ temp/for_examples.py
@@ -1,6 +1,2 @@
-# 1 день; +38 задача; всего: 38
-a = int(input())
-b = int(input())
-c = int(input())
-
-print('YES' if b - a == c - b else 'NO')
+# +39 задача; всего: 39  1 день;
+print(min(int(input()), int(input())))
