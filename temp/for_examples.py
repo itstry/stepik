6cfecfbac1e1a817d3590da9402f60b2b1685cdf
itstry <98533960+itstry@users.noreply.github.com>
2022-06-25 20:42:23 +0300

    Возрастная группа 4.1 Выбор из двух https://stepik.org/lesson/265081/step/1?unit=246029

--- temp/for_examples.py
+++ temp/for_examples.py
@@ -1,2 +1,10 @@
-# 1 день; +40 задача; всего: 40
-print(min(int(input()), int(input()), int(input()), int(input())))
+# 1 день; +41 задача; всего: 41
+age = int(input())
+if age <= 13:
+    print('детство')
+elif 14 <= age <= 24:
+    print('молодость')
+elif 25 <= age <= 59:
+    print('зрелость')
+else:
+    print('старость')
