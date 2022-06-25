6b50a4a6148be016990aa17448908ac552bbf21c
itstry <98533960+itstry@users.noreply.github.com>
2022-06-25 20:26:06 +0300

    Арифметические операции
    2.4 Целочисленная арифметика. Часть 1
    https://stepik.org/lesson/265079/step/11?unit=246027

--- temp/for_examples.py
+++ temp/for_examples.py
@@ -1,2 +1,4 @@
-# 1 день; +17 задача; всего: 17
-print((int(input()) + int(input()) + int(input()) + int(input())) * 3)
+# 1 день; +18 задача; всего: 18
+# put your python code here
+x, y = int(input()), int(input())
+print(f'{x} + {y} = {x + y}\n{x} - {y} = {x - y}\n{x} * {y} = {x * y}\n')
