f2ca7a664543682ab94281df6043c574988c29c6
itstry <98533960+itstry@users.noreply.github.com>
2022-06-25 20:58:52 +0300

    Наибольшее и наименьшее 6.1 Числовые типы данных: int, float https://stepik.org/lesson/265105/step/1?unit=246053

--- temp/for_examples.py
+++ temp/for_examples.py
@@ -1,4 +1,5 @@
-# хз какой день; +73 задача; всего: 73
+# хз какой день; +74задача; всего: 74
 
-i = float(input())
-print(i - int(i))
+numbers = a, b, c, d, e = int(input()), int(input()), int(input()), int(input()), int(input())
+print(f'Наименьшее число = {min(numbers)}')
+print(f'Наибольшее число = {max(numbers)}')
