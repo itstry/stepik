8590d6bce584e5248519d21df3ca686d9a666ccc
itstry <98533960+itstry@users.noreply.github.com>
2022-06-25 20:25:26 +0300

    Стоимость покупки 2.4 Целочисленная арифметика. Часть 1 https://stepik.org/lesson/265079/step/10?unit=246027

--- temp/for_examples.py
+++ temp/for_examples.py
@@ -1,4 +1,2 @@
-# 1 день; +16 задача; всего: 16
-# put your python code here
-x = int(input())
-print(f'Следующее за числом {x} число: {x + 1}\nДля числа {x} предыдущее число: {x - 1}')
+# 1 день; +17 задача; всего: 17
+print((int(input()) + int(input()) + int(input()) + int(input())) * 3)
