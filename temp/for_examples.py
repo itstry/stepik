9926fc3815f5fead0b6bb1664334b92618d19eb0
itstry <98533960+itstry@users.noreply.github.com>
2022-06-25 21:08:35 +0300

    Правильный многоугольник 6.3 Модуль math https://stepik.org/lesson/265110/step/2?unit=246058

--- temp/for_examples.py
+++ temp/for_examples.py
@@ -1,16 +1,5 @@
-# 4 день; +4 задача; всего: 92
+# 4 день; +5 задача; всего: 93
+from math import tan, pi
 
-
-from math import sqrt
-
-a, b, c = float(input()), float(input()), float(input())
-discriminant = b * b - 4 * a * c
-if discriminant < 0:
-    print('Нет корней')
-elif discriminant == 0:
-    print(-b / (2 * a))
-elif discriminant > -0:
-    x1 = (-b + sqrt(discriminant)) / (2 * a)
-    x2 = (-b - sqrt(discriminant)) / (2 * a)
-    quar_list = [x1, x2]
-    print(f'{min(quar_list)}\n{max(quar_list)}')
+n, a = float(input()), float(input())
+print((n * a ** 2) / (4 * tan(pi / n)))
