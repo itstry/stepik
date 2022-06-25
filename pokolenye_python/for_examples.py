4273bae8fd20b73677e2e1208f33cd9c8bfd204b
itstry <98533960+itstry@users.noreply.github.com>
2022-06-25 21:49:07 +0300

    Асимптотическое приближение 7.3 Частые сценарии https://stepik.org/lesson/294243/step/1?unit=275922

--- pokolenye_python/for_examples.py
+++ pokolenye_python/for_examples.py
@@ -1,6 +1,9 @@
-# 5 day; 12 задача; total: 109
-# github.com/itstry/stepik -- коммиты с моими решениями этого курса
-number_list = []
-for i in range(int(input())):
-    number_list.append(int(input()))
-print(sum(number_list))
+# 5 day; 13 задача; total: 110
+# ^-^ в профиле ссылка гитхаб
+from math import log
+
+i = int(input())
+total = 0
+for i in range(1, i + 1):
+    total += 1 / i
+print(total - log(i))
