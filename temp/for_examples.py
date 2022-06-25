981d4e05cea6319720a04d13084164daf307b8cd
itstry <98533960+itstry@users.noreply.github.com>
2022-06-25 20:43:38 +0300

    Принадлежность 1 4.2 Логические операции https://stepik.org/lesson/265083/step/1?unit=246031

--- temp/for_examples.py
+++ temp/for_examples.py
@@ -1,10 +1,2 @@
-# 1 день; +42 задача; всего: 42
-a, b, c = int(input()), int(input()), int(input())
-x = 0
-if a > 0:
-    x = x + a
-if b > 0:
-    x = x + b
-if c > 0:
-    x = x + c
-print(x)
+# 1 день; +43 задача; всего: 43
+print('Принадлежит' if -1 < int(input()) < 17 else 'Не принадлежит')
