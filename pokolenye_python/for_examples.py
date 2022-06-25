afe5cfb9896f2340a786316379173912a3d36b40
itstry <98533960+itstry@users.noreply.github.com>
2022-06-25 23:20:55 +0300

    Последовательность Фибоначчи 🌶️ 7.3 Частые сценарии https://stepik.org/lesson/294243/step/1?unit=275922

--- pokolenye_python/for_examples.py
+++ pokolenye_python/for_examples.py
@@ -1,9 +1,8 @@
-# 5 day; 20 задача; total: 117
+# 5 day; 21 задача; total: 118
 # ^-^ в профиле ссылка гитхаб
 
-flag = 'YES'
-for i in range(1, 11):
-    j = int(input())
-    if j % 2 != 0:
-        flag = "NO"
-print(flag)
+n = int(input())
+n_l = [0, 1, 1]
+for i in range(1, n+1):
+    n_l.append(n_l[i]+n_l[-1])
+    print(n_l[i], end=' ')
