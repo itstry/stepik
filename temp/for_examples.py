805e323a18dfcf412b96d0cd066d98266ad2261f
itstry <98533960+itstry@users.noreply.github.com>
2022-06-25 20:59:23 +0300

    Интересное число 6.1 Числовые типы данных: int, float https://stepik.org/lesson/265105/step/1?unit=246053

--- temp/for_examples.py
+++ temp/for_examples.py
@@ -1,4 +1,9 @@
-# хз какой день; +75 задача; всего: 75
+# хз какой день; +76 задача; всего: 76
 
-numbers = [int(input()), int(input()), int(input())]
-print(f'{sorted(numbers)[2]}\n{sorted(numbers)[1]}\n{sorted(numbers)[0]}')
+i = input()
+numbers = [int(i[0]), int(i[1]), int(i[2])]
+sorted(numbers)
+if max(numbers) - min(numbers) == (sum(numbers) - max(numbers) - min(numbers)):
+    print('Число интересное')
+else:
+    print('Число неинтересное')
