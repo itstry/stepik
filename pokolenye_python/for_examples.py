4931b54066083d004e429d03d076e7336f8be7c8
itstry <98533960+itstry@users.noreply.github.com>
2022-06-25 21:59:34 +0300

    Факториал 7.3 Частые сценарии https://stepik.org/lesson/294243/step/1?unit=275922

--- pokolenye_python/for_examples.py
+++ pokolenye_python/for_examples.py
@@ -1,8 +1,7 @@
-# 5 day; 14 задача; total: 111
+# 5 day; 15 задача; total: 112
 # ^-^ в профиле ссылка гитхаб
-i = int(input())
-total = 0
-for i in range(1, i + 1):
-    if (i**2)%10 == 2 or (i**2)%10 == 5 or (i**2)%10 == 8:
-        total += i
-print(total)
+
+result = 1
+for i in range(1, int(input()) + 1):
+    result *= i
+print(result)
