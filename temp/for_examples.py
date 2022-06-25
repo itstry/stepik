a704f0ef29da9898b58a0a6a30a2d6acb407f890
itstry <98533960+itstry@users.noreply.github.com>
2022-06-25 20:46:46 +0300

    Ход короля 🌶️ 4.2 Логические операции https://stepik.org/lesson/265083/step/1?unit=246031

--- temp/for_examples.py
+++ temp/for_examples.py
@@ -1,8 +1,9 @@
-# 1 день; +49 задача; всего: 49
+# 1 день; +50 задача; всего: 50
 x1, y1 = int(input()), int(input())
 x2, y2 = int(input()), int(input())
 
-if (x1 == x2 and y1 != y2) or (x1 != x2 and y1 == y2):
+if ((y1 + 1 == y2 or y1 - 1 == y2) and (x1 - 1 == x2 or x1 == x2 or x1 + 1 == x2)) or (
+        y2 == y1 and (x1 - 1 == x2 or x1 + 1 == x2)):
     print('YES')
 else:
     print('NO')
