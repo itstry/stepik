54a44f0cb12dff2e6441b5cd8b21eb65608d03d5
itstry <98533960+itstry@users.noreply.github.com>
2022-06-25 20:56:57 +0300

    Dog age 6.1 Числовые типы данных: int, float https://stepik.org/lesson/265105/step/1?unit=246053

--- temp/for_examples.py
+++ temp/for_examples.py
@@ -1,4 +1,15 @@
-# хз какой день; +70 задача; всего: 70
+# хз какой день; +71 задача; всего: 71
 
-f = float(input())
-print((5 / 9) * (f - 32))
+sobaken = float(input())
+if sobaken == 2:
+    i = 2
+    j = 0
+elif sobaken == 1:
+    i = 1
+    j = 0
+elif sobaken > 2:
+    i = 2
+    j = sobaken - 2
+else:
+    i = j = 0
+print(10.5 * (i) + 4 * j)
