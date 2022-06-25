d249f7ff202aa429364d0052a170d9f1cb607c26
itstry <98533960+itstry@users.noreply.github.com>
2022-06-25 21:05:47 +0300

    Евклидово расстояние
    6.3 Модуль math
    https://stepik.org/lesson/265110/step/2?unit=246058

--- temp/for_examples.py
+++ temp/for_examples.py
@@ -1,2 +1,5 @@
-# хз какой день; +86 задача; всего: 86
-print('YES' if '@' in (i := input()) and '.' in i else 'NO')
+# хз какой день; +87 задача; всего: 87
+from math import sqrt;
+
+x1, y1, x2, y2 = float(input()), float(input()), float(input()), float(input());
+print(sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2))  # 1 строка
