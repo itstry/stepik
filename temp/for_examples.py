a5f5b851badf0736f0d883aa6d3c33aa0df6f099
itstry <98533960+itstry@users.noreply.github.com>
2022-06-25 20:27:06 +0300

    Разделяй и властвуй
    2.4 Целочисленная арифметика. Часть 1
    https://stepik.org/lesson/265079/step/13?unit=246027

--- temp/for_examples.py
+++ temp/for_examples.py
@@ -1,3 +1,5 @@
-# 1 день; +19 задача; всего: 19
-# put your python code here
-print(int(input()) + int(input()) * (int(input()) - 1))
+# 1 день; +20 задача; всего: 20
+x = int(input())
+for z in range(1, 5):
+    print(x * z, end='---')
+print(x * 5)
