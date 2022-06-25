881c7093c3c1dae61eb6c50ef4c1528c10465442
itstry <98533960+itstry@users.noreply.github.com>
2022-06-25 21:19:59 +0300

    Звездный прямоугольник 7.1 Цикл for https://stepik.org/lesson/265118/step/1?unit=246067

--- temp/for_examples.py
+++ temp/for_examples.py
@@ -1,10 +1,4 @@
-# 4 day; 8 задача; total: 96
-# 4 день; +43 задача; всего: 147
-for _ in range(6):
-    print('AAA')
-for _ in range(5):
-    print('BBBB')
-print('E')
-for _ in range(9):
-    print('TTTTT')
-print("G")
+# 4 day; 9 задача; total: 97
+
+for _ in range(int(input())):
+    print('*' * 19)
