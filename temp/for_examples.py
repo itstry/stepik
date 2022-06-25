efa7ebec4f80f0213f86ef4b010ba7bb9122c1b9
itstry <98533960+itstry@users.noreply.github.com>
2022-06-25 20:52:20 +0300

    Начало столетия экзамен 5.1 Задачи на программирование https://stepik.org/lesson/292172/step/1?unit=273659

--- temp/for_examples.py
+++ temp/for_examples.py
@@ -1,11 +1,8 @@
-# 2 день; +58 задача; всего: 58
+# 2 день; +59 задача; всего: 59
 
-a1, b1, a2, b2, space = int(input()), int(input()), int(input()), int(input()), ' '
-if (a2 > b1 and a1 < b2) or (a1 > b2 and a2 < b1):
-    print('пустое множество')
-elif b1 == a2 or a1 == b2:
-    print(a1 if (a1 == b2) else a2)
-elif a1 <= a2:
-    print(str(a2) + space + str(b2) if (b1 >= b2) else str(a2) + space + str(b1))
-elif a1 >= a2:
-    print(str(a1) + space + str(b1) if (b1 <= b2) else str(a1) + space + str(b2))
+year = int(input())
+if year % 100 == 0:
+
+    print('YES')
+else:
+    print('NO')
