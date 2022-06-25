b16b277c7a0a1a62feae1f2fc5571afa4d16bd17
itstry <98533960+itstry@users.noreply.github.com>
2022-06-25 20:54:00 +0300

    Ход слона 🌶️ экзамен 5.1 Задачи на программирование https://stepik.org/lesson/292172/step/1?unit=273659

--- temp/for_examples.py
+++ temp/for_examples.py
@@ -1,3 +1,4 @@
-# 2 день; +63 задача; всего: 63
+# 2 день; +64 задача; всего: 64
 
-print('YES' if (((i := int(input())) % 2 != 0) or (i % 2 == 0 and i in range(6, 21))) else 'NO')
+x1, y1, x2, y2 = int(input()), int(input()), int(input()), int(input())
+print('YES' if ((x1 - x2) ** 2 == (y1 - y2) ** 2) else 'NO')
