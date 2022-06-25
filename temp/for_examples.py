e4a6f5c5107be125b82f7a891c98d88b8520d0a1
itstry <98533960+itstry@users.noreply.github.com>
2022-06-25 20:57:17 +0300

    Первая цифра после точки 6.1 Числовые типы данных: int, float https://stepik.org/lesson/265105/step/1?unit=246053

--- temp/for_examples.py
+++ temp/for_examples.py
@@ -1,15 +1,4 @@
-# хз какой день; +71 задача; всего: 71
+# хз какой день; +72 задача; всего: 72
 
-sobaken = float(input())
-if sobaken == 2:
-    i = 2
-    j = 0
-elif sobaken == 1:
-    i = 1
-    j = 0
-elif sobaken > 2:
-    i = 2
-    j = sobaken - 2
-else:
-    i = j = 0
-print(10.5 * (i) + 4 * j)
+i = float(input())
+print(int(i * 10 % 10))
