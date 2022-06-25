8dd7539ddaf4820da76d8b0bd34389a474ee35b6
itstry <98533960+itstry@users.noreply.github.com>
2022-06-25 21:36:21 +0300

    Сумма чисел 7.3 Частые сценарии https://stepik.org/lesson/294243/step/1?unit=275922

--- pokolenye_python/for_examples.py
+++ pokolenye_python/for_examples.py
@@ -1,5 +1,5 @@
 # 5 day; 12 задача; total: 109
-
+# github.com/itstry/stepik -- коммиты с моими решениями этого курса
 number_list = []
 for i in range(int(input())):
     number_list.append(int(input()))
