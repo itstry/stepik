c62874f6021cacfcdd85c66fde38491619aede0e
itstry <98533960+itstry@users.noreply.github.com>
2022-06-25 20:11:45 +0300

    Сумма чисел https://stepik.org/lesson/294243/step/6?thread=solutions&unit=275922

--- temp/for_examples.py
+++ temp/for_examples.py
@@ -1,6 +1,5 @@
 # 5 день; +11 задача; всего: 159
-counter = 0
-for i in range(int(input()), int(input()) + 1):
-    if (i ** 3) % 10 == 4 or (i ** 3) % 10 == 9:
-        counter += 1
-print(counter)
+number_list = []
+for i in range(int(input())):
+    number_list.append(int(input()))
+print(sum(number_list))
