3d337170b780e7b0310409c69751d175aed92621
itstry <98533960+itstry@users.noreply.github.com>
2022-06-25 22:20:52 +0300

    Знакочередующаяся сумма 7.3 Частые сценарии https://stepik.org/lesson/294243/step/1?unit=275922

--- pokolenye_python/for_examples.py
+++ pokolenye_python/for_examples.py
@@ -1,8 +1,10 @@
-# 5 day; 17 задача; total: 114
+# 5 day; 18 задача; total: 115
 # ^-^ в профиле ссылка гитхаб
 j = int(input())
-result = 0
-for i in range(1, j + 1):
-    if j % i == 0:
-        result += i
-print(result)
+result_1 = 0
+result_2 = 0
+for i in range(1, j + 1, 2):
+    result_1 += i
+for i in range(2, j + 1, 2):
+    result_2 += i
+print(result_1-result_2)
