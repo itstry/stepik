a0e0fef283518d8b2700369cafbabc9c73f5e94c
itstry <98533960+itstry@users.noreply.github.com>
2022-06-25 20:52:43 +0300

    Шахматная доска экзамен 5.1 Задачи на программирование https://stepik.org/lesson/292172/step/1?unit=273659

--- temp/for_examples.py
+++ temp/for_examples.py
@@ -1,8 +1,18 @@
-# 2 день; +59 задача; всего: 59
+# 2 день; +60 задача; всего: 60
 
-year = int(input())
-if year % 100 == 0:
+x1, y1, x2, y2 = int(input()), int(input()), int(input()), int(input())
 
+if (x1 + y1) % 2 == 0:
+    z1 = 1
+else:
+    z1 = 2
+
+if (x2 + y2) % 2 == 0:
+    z2 = 1
+else:
+    z2 = 2
+
+if z1 == z2:
     print('YES')
 else:
     print('NO')
