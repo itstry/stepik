5ea6fcfb22c6c6e116e73db8b49e624b372727d7
itstry <98533960+itstry@users.noreply.github.com>
2022-06-25 20:37:52 +0300

    Арифметическая прогрессия 4.1 Выбор из двух https://stepik.org/lesson/265081/step/1?unit=246029

--- temp/for_examples.py
+++ temp/for_examples.py
@@ -1,2 +1,6 @@
-# 1 день; +37 задача; всего: 37
-print('Доступ разрешен' if int(input()) >= 18 else 'Доступ запрещен')
+# 1 день; +38 задача; всего: 38
+a = int(input())
+b = int(input())
+c = int(input())
+
+print('YES' if b - a == c - b else 'NO')
