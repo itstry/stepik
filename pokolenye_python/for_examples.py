24cbbebf7f53358e936609c0e08f55d70130efde
itstry <98533960+itstry@users.noreply.github.com>
2022-06-27 11:15:01 +0300

    До КОНЦА 1 7.4 Цикл while https://stepik.org/lesson/265121/step/8?unit=246070

--- pokolenye_python/for_examples.py
+++ pokolenye_python/for_examples.py
@@ -1,8 +1,4 @@
-# 5 day; 21 задача; total: 118
+# 6 day; 1 задача; total: 119
 # ^-^ в профиле ссылка гитхаб
-
-n = int(input())
-n_l = [0, 1, 1]
-for i in range(1, n+1):
-    n_l.append(n_l[i]+n_l[-1])
-    print(n_l[i], end=' ')
+while (k := input()) != 'КОНЕЦ':
+    print(k)
