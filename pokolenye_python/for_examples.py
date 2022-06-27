e0fe7517cec224f59146e41b7fbf5d84388eef6f
itstry <98533960+itstry@users.noreply.github.com>
2022-06-27 17:57:08 +0300

    Обратный порядок 1
    7.5 Цикл while: обработка цифр числа
    https://stepik.org/lesson/265122/step/1?unit=246071

--- pokolenye_python/for_examples.py
+++ pokolenye_python/for_examples.py
@@ -1,21 +1,5 @@
-# 7 day; 6 задача; total: 125
-debt = int(input())
-coin_1 = coin_5 = coin_10 = coin_25 = 0
-while debt > 0:
-    if debt >= 25:
-        debt -= 25
-        coin_25 += 1
-        continue
-    if debt >= 10:
-        debt -= 10
-        coin_10 += 1
-        continue
-    if debt >= 5:
-        debt -= 5
-        coin_5 += 1
-        continue
-    if debt >= 1:
-        debt -= 1
-        coin_5 += 1
-        continue
-print(coin_1 + coin_5 + coin_10 + coin_25)
+# 7 day; 7 задача; total: 126
+num = int(input())
+while num != 0:
+    print(last_num := num % 10)
+    num //= 10
