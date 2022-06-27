11cc2da0caab8d0383c1ad5f64e3ee763046dd01
itstry <98533960+itstry@users.noreply.github.com>
2022-06-27 11:49:36 +0300

    Ведьмаку заплатите чеканной монетой
    7.4 Цикл while
    https://stepik.org/lesson/265121/step/8?unit=246070

--- pokolenye_python/for_examples.py
+++ pokolenye_python/for_examples.py
@@ -1,9 +1,21 @@
-# 7 day; 4 задача; total: 123
-numbers_list = []
-while True:
-    w = int(input())
-    if w >= 0:
-        numbers_list.append(w)
-    else:
-        print(sum(numbers_list))
-        break
+# 7 day; 6 задача; total: 125
+debt = int(input())
+coin_1 = coin_5 = coin_10 = coin_25 = 0
+while debt > 0:
+    if debt >= 25:
+        debt -= 25
+        coin_25 += 1
+        continue
+    if debt >= 10:
+        debt -= 10
+        coin_10 += 1
+        continue
+    if debt >= 5:
+        debt -= 5
+        coin_5 += 1
+        continue
+    if debt >= 1:
+        debt -= 1
+        coin_5 += 1
+        continue
+print(coin_1 + coin_5 + coin_10 + coin_25)
