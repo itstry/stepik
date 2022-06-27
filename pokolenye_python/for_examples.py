dd697097de5ff2e7a48baa9a72bc67ff94efc21e
itstry <98533960+itstry@users.noreply.github.com>
2022-06-27 18:06:49 +0300

    max и min
    7.5 Цикл while: обработка цифр числа
    https://stepik.org/lesson/265122/step/1?unit=246071

--- pokolenye_python/for_examples.py
+++ pokolenye_python/for_examples.py
@@ -1,5 +1,14 @@
-# 7 day; 8 задача; total: 127
+# 7 day; 9 задача; total: 128
 num = int(input())
+max_num = 0
+min_num = num % 10
 while num != 0:
-    print(last_num := num % 10, end='')
+    last_num = num % 10
+    if last_num > max_num:
+        max_num = last_num
+    if last_num < min_num:
+        min_num = last_num
+
     num //= 10
+print(f'Максимальная цифра равна {max_num}')
+print(f'Минимальная цифра равна {min_num}')
