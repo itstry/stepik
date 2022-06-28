130633d13ea4908205c9cbf1ce3e7b29765c62ad
itstry <98533960+itstry@users.noreply.github.com>
2022-06-28 10:56:17 +0300

    Упорядоченные цифры 🌶️
    7.5 Цикл while: обработка цифр числа
    https://stepik.org/lesson/265122/step/1?unit=246071

--- pokolenye_python/for_examples.py
+++ pokolenye_python/for_examples.py
@@ -1,10 +1,15 @@
-# 8 day; 3 задача; total: 131
+# 8 day; 4 задача; total: 132
 num = int(input())
 temp_num = num % 10
 flag = 'YES'
+num //= 10
 while num > 0:
     latest_num = num % 10
-    if latest_num != temp_num:
+    if latest_num - 1 != temp_num:
         flag = 'NO'
+        break
+    temp_num = num % 10
+
     num //= 10
+
 print(flag)
