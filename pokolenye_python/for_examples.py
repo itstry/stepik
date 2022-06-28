3cbf9ba211df1062278797d44bee101eae7cbfa4
itstry <98533960+itstry@users.noreply.github.com>
2022-06-28 10:25:06 +0300

    Одинаковые цифры
    7.5 Цикл while: обработка цифр числа
    https://stepik.org/lesson/265122/step/1?unit=246071

--- pokolenye_python/for_examples.py
+++ pokolenye_python/for_examples.py
@@ -1,21 +1,10 @@
-# 8 day; 1 задача; total: 129
+# 8 day; 2 задача; total: 130
 num = int(input())
-last_num = num % 10
-first_num = num
-sum_num = 0
-mult_num = 1
+first_2_num = num
 counter_len = 0
 while num > 0:
     latest_num = num % 10
-    if latest_num > 0:
-        sum_num = latest_num + sum_num
-        mult_num = latest_num * mult_num
+    if latest_num > -1:
         counter_len += 1
     num //= 10
-
-print(sum_num)  # сумма цифр
-print(counter_len)  # кол-во цифр
-print(mult_num)  # произведение цифр
-print(sum_num / counter_len)  # среднее арифметическое
-print(first_num := first_num // (10 ** (counter_len - 1)))  # первая цифра
-print(first_num + last_num)  # сумма 1 и последней цифр
+print((first_2_num//(10**(counter_len-2)))%10)
