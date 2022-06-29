42e559cdf6c7efe7afdc2734796a6e6416d1cc86
itstry <98533960+itstry@users.noreply.github.com>
2022-06-29 18:17:49 +0300

    154 task Делители-2
    7.9 Вложенные циклы. Часть 2
    https://stepik.org/lesson/298796/step/1?thread=solutions&unit=280623

--- pokolenye_python/for_examples.py
+++ pokolenye_python/for_examples.py
@@ -1,17 +1,8 @@
-a_num = int(input())
-b_num = int(input())
+num = int(input())
 
-max_div_now = max_div_out = 0  # делитель, внутренний и максимальный
-sum_div_now = sum_div_out = 0  # сумматор, внутренний и максимальный
-
-for i in range(a_num, b_num + 1):
+for i in range(1, num + 1):
+    print(i, end='')
     for k in range(1, i + 1):
-        max_div_now = i  # записываем делитель внутренний
         if i % k == 0:
-            sum_div_now += k
-    if sum_div_now >= sum_div_out and max_div_now > max_div_out:
-        max_div_out, sum_div_out = max_div_now, sum_div_now  # обмен переменными
-
-    max_div_now = counter_now = sum_div_now = 0  # обнуление внутренних переменных
-
-print(max_div_out, sum_div_out)
+            print('+', end='')
+    print()
