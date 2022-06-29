db00161d9b8bc9931702dec1adae5cd5ce59a0f3
itstry <98533960+itstry@users.noreply.github.com>
2022-06-29 12:21:07 +0300

    147 task Численный треугольник 1
    7.8 Вложенные циклы. Часть 1
    https://stepik.org/lesson/298795/step/1?unit=280622

--- pokolenye_python/for_examples.py
+++ pokolenye_python/for_examples.py
@@ -1,7 +1,5 @@
 num = int(input())
-num2 = num // 2
-
-for j in range(1, num2 + 1):
-    print('*' * j)
-for j in range(num2 + 1, 0, -1):
-    print('*' * j)
+for i in range(1, num + 1):
+    for j in range(i):
+        print(i, end='')
+    print()
