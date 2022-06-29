ae937201e4b40c82ff46f6d32d34aad44d7d34e6
itstry <98533960+itstry@users.noreply.github.com>
2022-06-29 12:38:18 +0300

    149 task Старинная задача
    7.8 Вложенные циклы. Часть 1
    https://stepik.org/lesson/298795/step/1?unit=280622

--- pokolenye_python/for_examples.py
+++ pokolenye_python/for_examples.py
@@ -1,8 +1,8 @@
 total = 0
-for n in range(1, 14):
-    for k in range(1, 13):
-        for m in range(1, 12):
-            if 28 * n + 30 * k + 31 * m == 365:
+for n in range(1, 11):
+    for k in range(1, 21):
+        for m in range(1, 201):
+            if 10 * n + 5 * k + 0.5 * m == 100:
                 total += 1
-                print('n =', n, 'k =', k, 'm =', m)
-print('Общее количество натуральных решений =', total)
+                if (n + k + m) == 100:
+                    print('n =', n, 'k =', k, 'm =', m)
