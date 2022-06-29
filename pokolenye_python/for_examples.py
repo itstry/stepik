e4512cb8866e0e96ddc66d9b97d8f9286c7ee2d9
itstry <98533960+itstry@users.noreply.github.com>
2022-06-29 12:29:08 +0300

    148 task 12 месяцев
    7.8 Вложенные циклы. Часть 1
    https://stepik.org/lesson/298795/step/1?unit=280622

--- pokolenye_python/for_examples.py
+++ pokolenye_python/for_examples.py
@@ -1,5 +1,8 @@
-num = int(input())
-for i in range(1, num + 1):
-    for j in range(i):
-        print(i, end='')
-    print()
+total = 0
+for n in range(1, 14):
+    for k in range(1, 13):
+        for m in range(1, 12):
+            if 28 * n + 30 * k + 31 * m == 365:
+                total += 1
+                print('n =', n, 'k =', k, 'm =', m)
+print('Общее количество натуральных решений =', total)
