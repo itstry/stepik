073efa7d6d290398ce05d5bbc5d7b1d72cf43e5a
itstry <98533960+itstry@users.noreply.github.com>
2022-06-29 16:48:06 +0300

    151 task Численный треугольник 3
    7.9 Вложенные циклы. Часть 2
    https://stepik.org/lesson/298796/step/1?thread=solutions&unit=280623

--- pokolenye_python/for_examples.py
+++ pokolenye_python/for_examples.py
@@ -1,16 +1,7 @@
-import time
-
-start_time = time.time()
-
-num_ex = [i ** 5 for i in range(0, 151)]
-print(num_ex)
-
-for a in range(1, 151):
-    for b in range(a, 151):
-        for c in range(b, 151):
-            for d in range(c, 151):
-                e = int((num_ex[a] + num_ex[b] + num_ex[c] + num_ex[d])**(1/5))
-                if (num_ex[a] + num_ex[b] + num_ex[c] + num_ex[d]) == e**5:
-                    print(f'{a} + {b} + {c} + {d} = {a + b + c + d + e}')
-                    print("time elapsed: {:.2f}s".format(time.time() - start_time))
-                    exit()
+num = int(input())
+end = int(((1 + num) / 2) * num)
+for i in range(1, num + 1):
+    i_end = int(((1 + i) / 2) * i)
+    for j in range((i_end - i) + 1, i_end + 1):
+        print(j, end=' ')
+    print()
