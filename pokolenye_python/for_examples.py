c29bd3fd1594a8628d3e0762b1f41aa92dab7974
itstry <98533960+itstry@users.noreply.github.com>
2022-06-29 16:48:21 +0300

    152 task Численный треугольник 4
    7.9 Вложенные циклы. Часть 2
    https://stepik.org/lesson/298796/step/1?thread=solutions&unit=280623

--- pokolenye_python/for_examples.py
+++ pokolenye_python/for_examples.py
@@ -1,7 +1,7 @@
 num = int(input())
-end = int(((1 + num) / 2) * num)
 for i in range(1, num + 1):
-    i_end = int(((1 + i) / 2) * i)
-    for j in range((i_end - i) + 1, i_end + 1):
-        print(j, end=' ')
+    for j in range(1, i):
+        print(j, end='')
+    for k in range(i, 0, -1):
+        print(k, end='')
     print()
