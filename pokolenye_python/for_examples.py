a70094c681bd52e8a916b0fff72ba8b4e9b5eec8
itstry <98533960+itstry@users.noreply.github.com>
2022-06-30 13:02:44 +0300

    163 task Все вместе 2
    8.2 Часть 2
    https://stepik.org/lesson/294080/step/1?thread=solutions&unit=275759

--- pokolenye_python/for_examples.py
+++ pokolenye_python/for_examples.py
@@ -1,1 +1,31 @@
-print(input()[2])
+num = int(input())
+
+latest_digit = num % 10
+
+counter_digit_3 = counter_latest_digit = counter_even = counter_digit_0_and_5 = 0
+sum_digit_greater_5, multi_digit_greater_7 = 0, 1
+
+while num > 0:
+    last_digit = num % 10
+
+    if last_digit == 3:
+        counter_digit_3 += 1
+    if last_digit == latest_digit:
+        counter_latest_digit += 1
+    if last_digit % 2 == 0:
+        counter_even += 1
+    if last_digit > 5:
+        sum_digit_greater_5 += last_digit
+    if last_digit > 7:
+        multi_digit_greater_7 *= last_digit
+    if last_digit == 0 or last_digit == 5:
+        counter_digit_0_and_5 += 1
+
+    num //= 10
+
+print(counter_digit_3)
+print(counter_latest_digit)
+print(counter_even)
+print(sum_digit_greater_5)
+print(multi_digit_greater_7)
+print(counter_digit_0_and_5)
