dfa2bf3e73216b6d172c5974fd8047681babe164
itstry <98533960+itstry@users.noreply.github.com>
2022-06-30 18:06:50 +0300

    175 task Decimal to Binary #2
    9.1 Индексация
    https://stepik.org/lesson/284101/step/5?unit=265440

--- pokolenye_python/for_examples.py
+++ pokolenye_python/for_examples.py
@@ -1,1 +1,8 @@
-print(str(bin(int(input())))[2:])
+num = int(input())
+num_in_bin = ''
+while num > 0:
+    integer = num // 2
+    remainder = num % 2
+    num_in_bin += str(remainder)
+    num = integer
+print(num_in_bin[::-1])
