6fde94e4a8adcd0504c5bc35db5367abd8f7ff7a
itstry <98533960+itstry@users.noreply.github.com>
2022-06-30 17:52:26 +0300

    173 task Гласные и согласные
    9.1 Индексация
    https://stepik.org/lesson/284101/step/5?unit=265440

--- pokolenye_python/for_examples.py
+++ pokolenye_python/for_examples.py
@@ -1,6 +1,14 @@
-line = input()
-counter_double = 0
-for i in range(len(line)-1):
-    if line[i] == line[i + 1]:
-        counter_double += 1
-print(counter_double)
+line = input().lower()
+
+vowel = 'ауоыиэяюёе'
+consonant = 'бвгджзйклмнпрстфхцчшщ'
+vowel_count = 0
+consonant_count = 0
+
+for i in vowel:
+    vowel_count += line.count(i)
+for i in consonant:
+    consonant_count += line.count(i)
+
+print(f'Количество гласных букв равно {vowel_count}')
+print(f'Количество согласных букв равно {consonant_count}')
