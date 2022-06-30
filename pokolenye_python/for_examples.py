d8e5b723031aaa051167e00a30302ceb2482fca3
itstry <98533960+itstry@users.noreply.github.com>
2022-06-30 16:55:38 +0300

    169 task Цифра 1
    9.1 Индексация
    https://stepik.org/lesson/284101/step/5?unit=265440

--- pokolenye_python/for_examples.py
+++ pokolenye_python/for_examples.py
@@ -1,5 +1,5 @@
-name = input()
-surname = input()
-middle_name = input()
-
-print(surname[0] + name[0] + middle_name[0])
+line = input()
+total = 0
+for i in range(len(line)):
+    total += int(line[i])
+print(total)
