bd4f39e068f026911d95b20d091c46bf4331940a
itstry <98533960+itstry@users.noreply.github.com>
2022-06-30 12:42:29 +0300

    161 task Звездная рамка
    8.2 Часть 2
    https://stepik.org/lesson/294080/step/1?thread=solutions&unit=275759

--- pokolenye_python/for_examples.py
+++ pokolenye_python/for_examples.py
@@ -1,13 +1,6 @@
-count = 0
-maximum = -10 ** 8 - 1
-for _ in range(4):
-    x = int(input())
-    if x % 2 != 0:
-        count += 1
-        if x > maximum:
-            maximum = x
-if count > 0:
-    print(count)
-    print(maximum)
-else:
-    print('NO')
+num = int(input())
+
+print(19 * '*')
+for i in range(num - 2):
+    print('*' + ' ' * 17 + '*')
+print(19 * '*')
