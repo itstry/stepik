f5079facb62d465e08b5ca7af4266af590cb722f
itstry <98533960+itstry@users.noreply.github.com>
2022-06-30 11:25:38 +0300

    155 task Цифровой корень
    7.9 Вложенные циклы. Часть 2
    https://stepik.org/lesson/298796/step/1?thread=solutions&unit=280623

--- pokolenye_python/for_examples.py
+++ pokolenye_python/for_examples.py
@@ -1,8 +1,10 @@
 num = int(input())
-
-for i in range(1, num + 1):
-    print(i, end='')
-    for k in range(1, i + 1):
-        if i % k == 0:
-            print('+', end='')
-    print()
+sum_num = 0
+while num > 9:
+    while num > 0:
+        last_digit = num % 10
+        sum_num += last_digit
+        num //= 10
+    num = sum_num
+    sum_num = 0
+print(num)
