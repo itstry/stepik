552da0c502d7a0de5dc49c50019f81b610770a75
itstry <98533960+itstry@users.noreply.github.com>
2022-06-30 11:39:24 +0300

    156 task Сумма факториалов
    7.9 Вложенные циклы. Часть 2
    https://stepik.org/lesson/298796/step/1?thread=solutions&unit=280623

--- pokolenye_python/for_examples.py
+++ pokolenye_python/for_examples.py
@@ -1,10 +1,9 @@
+from math import factorial
+
 num = int(input())
 sum_num = 0
-while num > 9:
-    while num > 0:
-        last_digit = num % 10
-        sum_num += last_digit
-        num //= 10
-    num = sum_num
-    sum_num = 0
-print(num)
+
+for i in range(1, num + 1):
+    sum_num = sum_num + factorial(i)
+
+print(sum_num)
