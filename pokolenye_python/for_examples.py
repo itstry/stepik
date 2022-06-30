fec2b8b837f27f35ee50de1f0fc4ca81361c60af
itstry <98533960+itstry@users.noreply.github.com>
2022-06-30 12:34:44 +0300

    158 task Ревью кода - 7 🌶️
    8.2 Часть 2
    https://stepik.org/lesson/294080/step/1?thread=solutions&unit=275759

--- pokolenye_python/for_examples.py
+++ pokolenye_python/for_examples.py
@@ -1,15 +1,7 @@
-num_1 = int(input())
-num_2 = int(input())
-
-counter_now = 0
-
-if num_1 == 1:
-    num_1 += 1
-
-for i in range(num_1, num_2 + 1):
-    counter_now = 0
-    for k in range(1, i + 1):
-        if i % k == 0:
-            counter_now += 1
-    if counter_now == 2:
-        print(i)
+num = int(input())
+sum = 0
+while num > 0:
+    if num % 2 == 0:
+        sum += num % 10
+    num //= 10
+print(sum)
