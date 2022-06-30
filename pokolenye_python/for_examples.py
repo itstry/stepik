b388713adf51bb6c9d8cb43c43281ed2c84f9ce1
itstry <98533960+itstry@users.noreply.github.com>
2022-06-30 16:32:38 +0300

    165 task Дополните приведенный код, используя индексатор, так чтобы он вывел символ запятой.
    9.1 Индексация
    https://stepik.org/lesson/284101/step/5?unit=265440

--- pokolenye_python/for_examples.py
+++ pokolenye_python/for_examples.py
@@ -1,6 +1,3 @@
-for a in range(1, 100000):
-    for c in range(1, a):
-        for d in range(1, c):
-            for b in range(1, d):
-                if a ** 3 + b ** 3 == c ** 3 + d ** 3:
-                    print(a**3 + b**3)
+s = "In 2010, someone paid 10k Bitcoin for two pizzas."
+
+print(s[7])
