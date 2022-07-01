2d827e941abb8fe73a7061a63acaeb180104c988
itstry <98533960+itstry@users.noreply.github.com>
2022-07-01 18:08:41 +0300

    210 task Второе вхождение
    10.2 Часть 2
    https://stepik.org/lesson/313233/step/1?unit=295750

--- pokolenye_python/for_examples.py
+++ pokolenye_python/for_examples.py
@@ -1,3 +1,13 @@
 line = input()
 
-print(line.replace('@', ''))
+if line.count('f') == 1:
+    print('-1')
+elif line.count('f') == 0:
+    print('-2')
+else:
+    index_f = line.find('f')
+    line = line[:index_f] + line[index_f + 1:]
+    if len(line) == 1:
+        print(1)
+        exit()
+    print(line.find('f') + 1)
