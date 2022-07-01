d8a0b428e795407128049d1057da172cd7ac1b30
itstry <98533960+itstry@users.noreply.github.com>
2022-07-01 19:02:44 +0300

    213 task Список чисел
    11.1 Введение в списки
    https://stepik.org/lesson/324750/step/1?unit=307926

--- pokolenye_python/for_examples.py
+++ pokolenye_python/for_examples.py
@@ -1,5 +1,2 @@
-line = input()
-index_1_h = line.find('h')
-index_2_h = line.rfind('h')
-
-print(line[:index_1_h + 1] + line[index_2_h - 1:index_1_h:-1] + line[index_2_h:])
+num = int(input())
+print(list(range(1, num + 1)))
