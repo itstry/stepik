7f741a912764dec9af058df5e232c175c7b377f6
itstry <98533960+itstry@users.noreply.github.com>
2022-07-02 19:49:34 +0300

    218 task Дополните приведенный код
    11.2 Основы работы со списками
    https://stepik.org/lesson/296419/step/1?unit=278139

--- pokolenye_python/for_examples.py
+++ pokolenye_python/for_examples.py
@@ -1,3 +1,4 @@
-primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71]
+evens = [2, 4, 6, 8, 10, 12, 14, 16, 18, 20]
+average = sum(evens) / len(evens)
 
-print(primes[:6])
+print(average)
