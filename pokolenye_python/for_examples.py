722d42d2d00f6a613b731dbebf41faab86f99755
itstry <98533960+itstry@users.noreply.github.com>
2022-07-03 19:08:20 +0300

    237 task Negatives, Zeros and Positives
    11.4 Вывод элементов списка
    https://stepik.org/lesson/328948/step/2?unit=312239

--- pokolenye_python/for_examples.py
+++ pokolenye_python/for_examples.py
@@ -1,10 +1,12 @@
-list_sentence = ([input() for i in range(int(input()))])
-list_keyword = ([input() for k in range(int(input()))])
-
-for m in range(len(list_sentence)):
-    counter_in = 0
-    for z in range(len(list_keyword)):
-        if list_keyword[z].lower() in (list_sentence[m].lower()):
-            counter_in += 1
-    if counter_in == (len(list_keyword)):
-        print(list_sentence[m])
+list_num = ([int(input()) for _ in range(int(input()))])
+list_neg, list_zero, list_posit = [], [], []
+for i in list_num:
+    if i < 0:
+        list_neg.append(i)
+    elif i == 0:
+        list_zero.append(i)
+    else:
+        list_posit.append(i)
+print(*list_neg, sep='\n')
+print(*list_zero, sep='\n')
+print(*list_posit, sep='\n')
