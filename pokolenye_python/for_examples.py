923e2eb571fbade4618e8db255dd04c867e4bdfa
itstry <98533960+itstry@users.noreply.github.com>
2022-07-03 16:47:35 +0300

    230 task Символы всех строк
    11.3 Методы списков. Часть 1
    https://stepik.org/lesson/327207/step/6?unit=310501

--- pokolenye_python/for_examples.py
+++ pokolenye_python/for_examples.py
@@ -1,5 +1,4 @@
-list_words = [input() for i in range(int(input()))]
-k = int(input())
-for words in list_words:
-    if len(words) >= k:
-        print(words[k - 1], end='')
+list_ch = []
+for i in range(int(input())):
+    list_ch.extend(input())
+print(list_ch)
