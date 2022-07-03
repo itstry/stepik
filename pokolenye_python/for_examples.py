9209b7f5d05fe57170bf1f901bc3f5403bef229e
itstry <98533960+itstry@users.noreply.github.com>
2022-07-03 16:12:09 +0300

    228 task Суммы двух
    11.3 Методы списков. Часть 1
    https://stepik.org/lesson/327207/step/6?unit=310501

--- pokolenye_python/for_examples.py
+++ pokolenye_python/for_examples.py
@@ -1,2 +1,4 @@
 list_num = [int(input()) for i in range(int(input()))]
-print([(list_num[k] + list_num[k + 1]) for k in range(len(list_num)) if k != (list_num.index(list_num[-1]))])
+for i in range(len(list_num) // 2):
+    del list_num[i + 1]
+print(list_num)
