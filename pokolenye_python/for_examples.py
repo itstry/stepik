8d7b22ed8bbbf107900e5a8da542c949e03d75f5
itstry <98533960+itstry@users.noreply.github.com>
2022-07-03 12:10:45 +0300

    222 task Все сразу 1 🌶️
    11.3 Методы списков. Часть 1
    https://stepik.org/lesson/327207/step/6?unit=310501

--- pokolenye_python/for_examples.py
+++ pokolenye_python/for_examples.py
@@ -1,5 +1,7 @@
-numbers1 = [1, 2, 3]
-numbers2 = [6]
-numbers3 = [7, 8, 9, 10, 11, 12, 13]
-
-print(numbers1*2+numbers2*9+numbers3)
+numbers = [2, 6, 3, 14, 10, 4, 11, 16, 12, 5, 4, 16, 1, 0, 8, 16, 10, 10, 8, 5, 1, 11, 10, 10, 12, 0, 0, 6, 14, 8, 2,
+           12, 14, 5, 6, 12, 1, 2, 10, 14, 9, 1, 15, 1, 2, 14, 16, 6, 7, 5]
+print(len(numbers))
+print(numbers[-1])
+print(numbers[::-1])
+print('YES' if 5 and 17 in numbers else 'NO')
+print(numbers[1:-1])
