b4880c486efab682067adc3611e96f900ba4c841
itstry <98533960+itstry@users.noreply.github.com>
2022-07-03 16:55:02 +0300

    231 task Дополните приведенный код
    11.4 Вывод элементов списка
    https://stepik.org/lesson/328948/step/2?unit=312239

--- pokolenye_python/for_examples.py
+++ pokolenye_python/for_examples.py
@@ -1,4 +1,2 @@
-list_ch = []
-for i in range(int(input())):
-    list_ch.extend(input())
-print(list_ch)
+numbers = [1, 78, 23, -65, 99, 9089, 34, -32, 0, -67, 1, 11, 111]
+print(sum([i ** 2 for i in numbers]))
