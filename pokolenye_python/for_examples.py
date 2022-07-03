f1cc32651b8b2914a91419095190690ec3ff9ce7
itstry <98533960+itstry@users.noreply.github.com>
2022-07-03 18:19:43 +0300

    236 task Google search - 2 🌶️🌶️
    11.4 Вывод элементов списка
    https://stepik.org/lesson/328948/step/2?unit=312239

--- pokolenye_python/for_examples.py
+++ pokolenye_python/for_examples.py
@@ -1,3 +1,10 @@
 list_sentence = ([input() for i in range(int(input()))])
-key_word = input().lower()
-print(*[list_sentence[k] for k in range(len(list_sentence)) if key_word in list_sentence[k].lower()], sep='\n')
+list_keyword = ([input() for k in range(int(input()))])
+
+for m in range(len(list_sentence)):
+    counter_in = 0
+    for z in range(len(list_keyword)):
+        if list_keyword[z].lower() in (list_sentence[m].lower()):
+            counter_in += 1
+    if counter_in == (len(list_keyword)):
+        print(list_sentence[m])
