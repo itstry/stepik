9ec1b466d65a5002f86e19c694acc169a7f09b67
itstry <98533960+itstry@users.noreply.github.com>
2022-07-03 17:29:30 +0300

    234 task Без дубликатов
    11.4 Вывод элементов списка
    https://stepik.org/lesson/328948/step/2?unit=312239

--- pokolenye_python/for_examples.py
+++ pokolenye_python/for_examples.py
@@ -1,3 +1,6 @@
-list_num = ([int(input()) for i in range(int(input()))])
-del list_num[list_num.index(max(list_num))], list_num[list_num.index(min(list_num))]
-print(*list_num, sep='\n')
+list_word = ([input() for i in range(int(input()))])
+list_new = []
+for k in list_word:
+    if k not in list_new:
+        list_new.append(k)
+print(*list_new, sep='\n')
