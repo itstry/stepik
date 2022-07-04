6e7e46122c734df576ee98d088c5d4404f0b8e25
itstry <98533960+itstry@users.noreply.github.com>
2022-07-04 10:30:50 +0300

    242 task Корректный ip-адрес
    11.5 Методы строк: split, join
    https://stepik.org/lesson/324755/step/3?unit=307931

--- pokolenye_python/for_examples.py
+++ pokolenye_python/for_examples.py
@@ -1,2 +1,6 @@
-list_num = ([int(i) for i in input().split()])
-print(*[n * '+' for n in list_num], sep='\n')
+list_num = ([int(i) for i in input().split('.')])
+flag = 'ДА'
+for i in list_num:
+    if i > 255:
+        flag = 'НЕТ'
+print(flag)
