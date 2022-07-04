6b5b3d8fcba686474a2949c8c6a0cf69f983c422
itstry <98533960+itstry@users.noreply.github.com>
2022-07-04 09:55:58 +0300

    238 task Построчный вывод
    11.5 Методы строк: split, join
    https://stepik.org/lesson/324755/step/3?unit=307931

--- pokolenye_python/for_examples.py
+++ pokolenye_python/for_examples.py
@@ -1,12 +1,3 @@
-list_num = ([int(input()) for _ in range(int(input()))])
-list_neg, list_zero, list_posit = [], [], []
-for i in list_num:
-    if i < 0:
-        list_neg.append(i)
-    elif i == 0:
-        list_zero.append(i)
-    else:
-        list_posit.append(i)
-print(*list_neg, sep='\n')
-print(*list_zero, sep='\n')
-print(*list_posit, sep='\n')
+line = input()
+list_line = line.split()
+print(*list_line, sep='\n')
