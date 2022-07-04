bbcb800206f2df553c45d800e1e4580a85e9b365
itstry <98533960+itstry@users.noreply.github.com>
2022-07-04 17:55:29 +0300

    244 task Количество совпадающих пар
    11.5 Методы строк: split, join
    https://stepik.org/lesson/324755/step/3?unit=307931

--- pokolenye_python/for_examples.py
+++ pokolenye_python/for_examples.py
@@ -1,2 +1,8 @@
-line, separator = input(), input()
-print(separator.join(line))
+list_line = input().split()
+counter_pair = 0
+
+for k in range(len(list_line)):
+    for i in range(k+1, len(list_line)):
+        if list_line[k] == list_line[i]:
+            counter_pair += 1
+print(counter_pair)
