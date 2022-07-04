eefcc18c853a87aa7576915a83755c6b48b2757d
itstry <98533960+itstry@users.noreply.github.com>
2022-07-04 14:48:40 +0300

    243 task Добавь разделитель
    11.5 Методы строк: split, join
    https://stepik.org/lesson/324755/step/3?unit=307931

--- pokolenye_python/for_examples.py
+++ pokolenye_python/for_examples.py
@@ -1,6 +1,2 @@
-list_num = ([int(i) for i in input().split('.')])
-flag = 'ДА'
-for i in list_num:
-    if i > 255:
-        flag = 'НЕТ'
-print(flag)
+line, separator = input(), input()
+print(separator.join(line))
