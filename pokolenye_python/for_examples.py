6b7017b304717cc277359bef8212b807a2245284
itstry <98533960+itstry@users.noreply.github.com>
2022-07-05 15:37:46 +0300

    255 task Дополните
    11.7 Списочные выражения
    https://stepik.org/lesson/326725/step/3?thread=solutions&unit=310010

--- pokolenye_python/for_examples.py
+++ pokolenye_python/for_examples.py
@@ -1,7 +1,1 @@
-keywords = ['False', 'True', 'None', 'and', 'with', 'as', 'assert', 'break', 'class', 'continue', 'def', 'del', 'elif',
-            'else', 'except', 'finally', 'try', 'for', 'from', 'global', 'if', 'import', 'in', 'is', 'lambda',
-            'nonlocal', 'not', 'or', 'pass', 'raise', 'return', 'while', 'yield']
-
-new_keywords = [i for i in keywords if len(i) >= 5]
-
-print(new_keywords)
+print([i for i in range(100, 1000) if str(i) == str(i)[::-1]])
