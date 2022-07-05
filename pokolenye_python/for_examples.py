a359a62f50fee886f8f6d3e5857a0e92e9f4e152
itstry <98533960+itstry@users.noreply.github.com>
2022-07-05 15:02:43 +0300

    251 task Сортировка чисел
    11.6 Методы списков. Часть 2
    https://stepik.org/lesson/324754/step/6?thread=solutions&unit=307930

--- pokolenye_python/for_examples.py
+++ pokolenye_python/for_examples.py
@@ -1,2 +1,5 @@
-list_str = ([input() for _ in range(int(input().strip('#')))])
-print(*((i[:i.index('#')].rstrip()) if '#' in i else i for i in list_str), sep='\n')
+list_num = [int(i) for i in input().split()]
+list_num.sort()
+print(*list_num)
+list_num.sort(reverse=True)
+print(*list_num)
