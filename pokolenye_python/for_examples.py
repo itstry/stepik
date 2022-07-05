c83ff070d756dec48aa5ce455ec4569eab779836
itstry <98533960+itstry@users.noreply.github.com>
2022-07-05 12:33:42 +0300

    245 task Все сразу 2 🌶️
    11.5 Методы строк: split, join
    https://stepik.org/lesson/324755/step/3?unit=307931

--- pokolenye_python/for_examples.py
+++ pokolenye_python/for_examples.py
@@ -1,8 +1,10 @@
-list_line = input().split()
-counter_pair = 0
+numbers = [8, 9, 10, 11]
 
-for k in range(len(list_line)):
-    for i in range(k+1, len(list_line)):
-        if list_line[k] == list_line[i]:
-            counter_pair += 1
-print(counter_pair)
+del numbers[1]  # 1
+numbers.insert(1, 17)
+numbers = numbers + [4, 5, 6]  # 2
+del numbers[0]  # 3
+numbers = numbers * 2  # 4
+numbers.insert(3, 25)  # 5
+
+print(numbers)  # 6
