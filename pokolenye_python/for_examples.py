3f20c7fc0be638e9571d71e5afa0a43b39f1e059
itstry <98533960+itstry@users.noreply.github.com>
2022-07-05 16:29:55 +0300

    264 task Сумма двух списков
    12.2 Часть 2
    https://stepik.org/lesson/327221/step/1?thread=solutions&unit=310520

--- pokolenye_python/for_examples.py
+++ pokolenye_python/for_examples.py
@@ -1,1 +1,3 @@
-print([i for i in range(1, int(input()) + 1) if i % 2 == 0])
+list_l = [int(i) for i in input().split()]
+list_m = [int(k) for k in input().split()]
+print(*(list_m[j] + list_l[j] for j in range(len(list_l))))
