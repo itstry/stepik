600611d7acac43cf346cd9fe145f63530f25c3e2
itstry <98533960+itstry@users.noreply.github.com>
2022-07-05 15:22:01 +0300

    252 task Дополните
    11.7 Списочные выражения
    https://stepik.org/lesson/326725/step/3?thread=solutions&unit=310010

--- pokolenye_python/for_examples.py
+++ pokolenye_python/for_examples.py
@@ -1,5 +1,7 @@
-list_num = [int(i) for i in input().split()]
-list_num.sort()
-print(*list_num)
-list_num.sort(reverse=True)
-print(*list_num)
+keywords = ['False', 'True', 'None', 'and', 'with', 'as', 'assert', 'break', 'class', 'continue', 'def', 'del', 'elif',
+            'else', 'except', 'finally', 'try', 'for', 'from', 'global', 'if', 'import', 'in', 'is', 'lambda',
+            'nonlocal', 'not', 'or', 'pass', 'raise', 'return', 'while', 'yield']
+
+new_keywords = [i[1:] for i in keywords]
+
+print(new_keywords)
