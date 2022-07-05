39a16368ca9d8e88b7cd7cdbac5dd449346ddf3d
itstry <98533960+itstry@users.noreply.github.com>
2022-07-05 16:34:45 +0300

    265 task Сумма чисел
    12.2 Часть 2
    https://stepik.org/lesson/327221/step/1?thread=solutions&unit=310520

--- pokolenye_python/for_examples.py
+++ pokolenye_python/for_examples.py
@@ -1,3 +1,2 @@
-list_l = [int(i) for i in input().split()]
-list_m = [int(k) for k in input().split()]
-print(*(list_m[j] + list_l[j] for j in range(len(list_l))))
+str_in = input().replace(' ', '+')
+print(f'{str_in}={eval(str_in)}')
